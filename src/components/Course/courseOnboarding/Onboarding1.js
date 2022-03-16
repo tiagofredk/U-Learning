ec3aaa27cdd71dc83551843a1e2f66677b9033ff
Manoeto DCI <manoetouazar@gmail.com>
2022-03-16 09:10:06 +0100

    manoel160322

--- src/components/Course/courseOnboarding/Onboarding1.js
+++ src/components/Course/courseOnboarding/Onboarding1.js
@@ -4,15 +4,25 @@ import { StyleSheet, Text, View, FlatList, Animated, Button } from 'react-native
 import slides from './slides'
 import OnboardItem from './OnboardItem'
 import Paginator from './Paginator'
-import Tabnavigator from "../../navigation/Tabnavigator";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
 
+
+const tab = createBottomTabNavigator()
 
 const Onboarding1 = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  
+
   const scrollX = useRef(new Animated.Value(0)).current;
 
   const slidesRef = useRef(null);
+
+  const navigation = useNavigation();
 
   const viewableItemsChanged = useRef(({viewableItems}) => {
     setCurrentIndex(viewableItems[0].index);
@@ -42,7 +52,22 @@ const Onboarding1 = () => {
       />
       
       <Paginator data={slides} scrollX={scrollX} />
-      <Text>Create a button to home and Courses</Text>
+
+      {/*/ from here bottom navigation bar workspace*/}
+      <View style={styles.tab}>
+          
+        <Text style={styles.button} onPress={()=> navigation.navigate ("Courses") }>
+          <MaterialIcons name="storefront" size={40}  color={'red'} /></Text>
+        <Text style={styles.button} onPress={()=> navigation.navigate ("Chat") }>
+          <AntDesign name="wechat" size={40} color="red" />
+        </Text>
+        <Text style={styles.button} onPress={()=> navigation.navigate ("News") }>
+          <Entypo name="news" size={40} color="red" /></Text>
+        <Text style={styles.button} onPress={()=> navigation.navigate ("User") }>
+          <AntDesign name="user" size={40} color="red" />
+        </Text>
+      </View>
+
     </View>
 
     </>
@@ -51,4 +76,20 @@ const Onboarding1 = () => {
 
 export default Onboarding1
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  tab:{
+    backgroundColor: '#646464',
+    flexDirection: 'row',
+    padding: 10, 
+    alignItems: 'stretch', 
+    textAlign: 'center',
+  },
+  courses:{
+    backgroundColor: 'red',
+    borderRadius: 4,
+    fontWeight:'bold',
+    fontSize: 20,
+    color: 'white',
+    textAlign: "center",
+  },
+})
