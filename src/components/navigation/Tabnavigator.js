add49937f7ce8b636ed4b6689871f0042402bdf0
Peer Gehrts <gehrts.peer@gmail.com>
2022-03-17 11:15:27 +0100

    added Impressum

--- src/components/navigation/Tabnavigator.js
+++ src/components/navigation/Tabnavigator.js
@@ -1,49 +1,73 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react'
-import MainPage from '../Home/MainPage';
-import Courses from '../Course/Courses';
+import { StyleSheet, Text, View, Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import MainPage from "../Home/MainPage";
+import Courses from "../Course/Courses";
 // import Cart from '../Cart/Cart'
-import UserProfile from "../User/UserProfile"
-import { Ionicons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+import UserProfile from "../User/UserProfile";
+import Impressum from "../Impressum/Impressum";
+import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
 // import {HomeStack, CoursesStack} from './StackNavigator';
 
 const Tabnavigator = () => {
-  
-    const Tab = createBottomTabNavigator();
-  
-    return (
-    <Tab.Navigator screenOptions={{ 
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
         headerShown: false,
-        tabBarShowLabel:true
-      }}>
-        <Tab.Screen name="Home" component={MainPage} options={{
-            tabBarIcon: ({color, size}) => (
-               <Ionicons name="ios-home-outline" size={size} color={'#646464'} />
-                )
-        }} />
-        <Tab.Screen name="Courses" component={Courses} options={{
-            tabBarIcon: ({color, size}) => (
-                <MaterialIcons name="storefront" size={size} color={'#646464'} />
-            )
-        }}/>
-        {/*
+        tabBarShowLabel: true,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={MainPage}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-home-outline" size={size} color={"#646464"} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Courses"
+        component={Courses}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="storefront" size={size} color={"#646464"} />
+          ),
+        }}
+      />
+      {/*
         <Tab.Screen name="Cart" component={Cart} options={{
             tabBarIcon: ({color, size}) => (
                 <AntDesign name="shoppingcart" size={size} color={color} />
             )
         }}/> */}
-        <Tab.Screen name="Account" component={UserProfile} options={{
-            tabBarIcon: ({color, size}) => (
-                <AntDesign name="user" size={size} color={'#646464'} />
-            )
-        }}/>
-      </Tab.Navigator>
-  )
-}
+      <Tab.Screen
+        name="Account"
+        component={UserProfile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" size={size} color={"#646464"} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Impressum"
+        component={Impressum}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="law" size={size} color={"#646464"} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default Tabnavigator
+export default Tabnavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
